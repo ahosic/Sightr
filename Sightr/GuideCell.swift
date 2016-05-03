106c3d6c044161c5af8a1e60ad2310ad7d02374b
AlminHosic <almin.hosic@gmail.com>
2016-05-03 12:09:24 +0200

    Added ID of guide

--- Sightr/GuideCell.swift
+++ Sightr/GuideCell.swift
@@ -1,6 +1,8 @@
 import UIKit
 
 class GuideCell: UITableViewCell {
+    var guideID:String?
     @IBOutlet weak var name: UILabel!
     @IBOutlet weak var itemsCount: UILabel!
+    @IBOutlet weak var activationStatus: UISwitch!
 }
