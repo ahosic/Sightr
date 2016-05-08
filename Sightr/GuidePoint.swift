c3c442b2f2d87a04fafb0e8834cce37b37254b87
AlminHosic <almin.hosic@gmail.com>
2016-05-08 19:43:51 +0200

    Removed UIImage object; Added imageName property; Added constructor and method for serialization/deserialization

--- Sightr/GuidePoint.swift
+++ Sightr/GuidePoint.swift
@@ -2,6 +2,7 @@ import Foundation
 import UIKit
 import CoreLocation
 import RealmSwift
+import SwiftyJSON
 
 class GuidePoint : Object {
     dynamic var id:String = ""
@@ -23,7 +24,7 @@ class GuidePoint : Object {
     dynamic var address:String? = nil
     
     dynamic var hasImage:Bool = false
-    var image:UIImage?
+    dynamic var imageName:String? = nil
     dynamic var text:String = ""
     dynamic var link:String? = nil
     
@@ -35,9 +36,8 @@ class GuidePoint : Object {
         return "id"
     }
     
-    convenience init(name:String, location:CLLocationCoordinate2D, address:String?, radius:Double, text:String, link:String, image:UIImage?){
+    convenience init(name:String, location:CLLocationCoordinate2D, address:String?, radius:Double, text:String, link:String?, imageName:String?){
         self.init()
-        
         self.id = NSUUID().UUIDString
         self.name = name
         
@@ -46,16 +46,69 @@ class GuidePoint : Object {
         
         self.text = text
         self.link = link
-        self.image = image
         
         self.longitude = 0
         self.latitude = 0
         self.location = location
+        self.imageName = imageName
         
-        if image != nil {
+        if imageName != nil {
             self.hasImage = true
         } else {
             self.hasImage = false
         }
+    }
+    
+    convenience init(json:JSON) {
+        self.init()
+        
+        self.id = json["id"].stringValue
+        self.name = json["name"].stringValue
+        
+        self.address = json["address"].string
+        self.radius = json["radius"].doubleValue
+        
+        self.text = json["text"].stringValue
+        self.link = json["link"].string
+        self.hasImage = json["hasImage"].boolValue
+        self.imageName = json["imageName"].string
+        self.longitude = json["longitude"].doubleValue
+        self.latitude = json["latitude"].doubleValue
+    }
+    
+    func toJSON() -> String {
+        let pointID = NSUUID().UUIDString
+        
+        var json = "{"
+        
+        json += "\"id\": \"\(pointID)\","
+        json += "\"name\": \"\(self.name)\","
+        json += "\"latitude\": \"\(self.latitude)\","
+        json += "\"longitude\": \"\(self.longitude)\","
+        json += "\"radius\": \"\(self.radius)\","
+        json += "\"text\": \"\(self.text)\","
+        json += "\"hasImage\": \"\(self.hasImage)\","
+        
+        if imageName != nil {
+            json += "\"imageName\": \"\(self.imageName!)\","
+        } else {
+            json += "\"imageName\": null,"
+        }
+        
+        if address != nil {
+            json += "\"address\": \"\(self.address!)\","
+        } else {
+            json += "\"address\": null,"
+        }
+        
+        if link != nil {
+            json += "\"link\": \"\(self.link!)\""
+        } else {
+            json += "\"link\": null"
+        }
+        
+        json += "}"
+        
+        return json
     }
 }
