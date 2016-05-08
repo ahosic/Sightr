64a587a701b0161393b057df762773615e6179e2
AlminHosic <almin.hosic@gmail.com>
2016-05-08 19:44:52 +0200

    Added constructor and method for serialization/deserialization

--- Sightr/Guide.swift
+++ Sightr/Guide.swift
@@ -1,5 +1,6 @@
 import Foundation
 import RealmSwift
+import SwiftyJSON
 
 class Guide : Object {
     dynamic var id:String = ""
@@ -28,5 +29,54 @@ class Guide : Object {
         self.name = name
         self.isActivated = false
         self.timestamp = NSDate()
+    }
+    
+    convenience init(data:NSData) {
+        self.init()
+        
+        let json = JSON(data: data)
+        
+        self.id = json["id"].stringValue
+        self.name = json["name"].stringValue
+        self.isActivated = json["isActivated"].boolValue
+        
+        let dateFormatter = NSDateFormatter()
+        if let time = dateFormatter.dateFromString(json["timestamp"].stringValue) {
+            self.timestamp = time
+        } else {
+            self.timestamp = NSDate()
+        }
+        
+        let jsonPoints = json["points"].arrayValue
+        for jsonPoint in jsonPoints {
+            let point = GuidePoint(json: jsonPoint)
+            points.append(point)
+        }
+    }
+    
+    func toJSON() -> String {
+        let guideID = NSUUID().UUIDString
+        
+        var json = "{"
+        
+        json += "\"id\": \"\(guideID)\","
+        json += "\"name\": \"\(self.name)\","
+        json += "\"isActivated\": \"\(false)\","
+        json += "\"timestamp\": \"\(self.timestamp)\","
+        json += "\"points\": ["
+        
+        var firstPoint:Bool = true
+        for point in self.points {
+            if !firstPoint {
+                json += ","
+            }
+            
+            json += point.toJSON()
+            firstPoint = false
+        }
+        
+        json += "]}"
+        
+        return json
     }
 }
